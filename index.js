154566e2fdafbce9bc506cfd13188719ab36ea48
maxlestage <maxlestage@icloud.com>
2022-04-11 17:52:27 +0200

    Add MongoDB 🤩

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const cors = require("cors");
+const Note = require("./models/note");
 app.use(cors());
-
-app.use(express.static("build"));
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true,
-  },
-];
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -68,8 +48,10 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
