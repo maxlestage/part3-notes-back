f2b3bd3a8a00bca36c227a4ee3b9ea6bab8bd2e9
maxlestage <maxlestage@icloud.com>
2022-04-13 11:48:22 +0200

    update : app.get("/api/notes/:id"

--- index.js
+++ index.js
@@ -22,11 +22,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
@@ -45,23 +40,25 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).end();
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
-
   response.status(204).end();
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
 });
 
 const unknownEndpoint = (request, response) => {
