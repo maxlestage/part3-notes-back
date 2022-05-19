f1552dca6f11e7026c52eb10170936783972e22b
maxlestage <maxlestage@icloud.com>
2022-05-19 16:51:53 +0200

    Up ✅

--- index.js
+++ index.js
@@ -1,99 +1,10 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const cors = require("cors");
-const Note = require("./models/note");
-const errorHandler = require("./errorHandler");
-app.use(cors());
+const app = require("./app"); // the actual Express application
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-// const requestLogger = (request, response, next) => {
-//   console.log("Method:", request.method);
-//   console.log("Path:  ", request.path);
-//   console.log("Body:  ", request.body);
-//   console.log("---");
-//   next();
-// };
+const server = http.createServer(app);
 
-app.use(express.json());
-// app.use(requestLogger);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1><h2>Updated!</h2>");
-});
-
-// Affichage des notes :
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
-
-// Affichage d'une note via son id :
-app.get("/api/notes/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-// Création d'une note via son id :
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
-});
-
-//Update d'une note via son id :
-app.put("/api/notes/:id", (request, response, next) => {
-  const { content, important } = request.body;
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-
-// Suppression d'une note via son id :
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-// Ne pointe pas vers une route existante :
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-app.use(unknownEndpoint);
-
-// L'id demandé n'est pas repertorié dans la base de données :
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
